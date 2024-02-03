78c542ce8c816a1c59d9a7569e3c8ad8d3de767e
yuum1276 <dyalsla0105@gmail.com>
2024-02-03 23:53:07 +0900

    73. 원하는 문자열 찾기

--- Lv.0/71to80/73. 원하는 문자열 찾기.js
+++ Lv.0/71to80/73. 원하는 문자열 찾기.js
@@ -3,6 +3,7 @@
 */
 
 function solution(myString, pat) {
-    var answer = 0;
-    return answer;
+    function solution(myString, pat) {
+        return myString.toUpperCase().includes(pat.toUpperCase()) ? 1:0 
+    }
 }
