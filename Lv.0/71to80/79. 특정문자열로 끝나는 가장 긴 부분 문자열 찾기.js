048684e4cdce154b8f32cbe0fbcdede51c57176d
JoYuMi <97943318+JoYouMe@users.noreply.github.com>
2024-02-16 16:58:52 +0900

    79. 특정문자열로 끝나는 가장 긴 부분 문자열 찾기

--- Lv.0/71to80/79. 특정문자열로 끝나는 가장 긴 부분 문자열 찾기.js
+++ Lv.0/71to80/79. 특정문자열로 끝나는 가장 긴 부분 문자열 찾기.js
@@ -2,9 +2,9 @@
  * 문자열 myString과 pat가 주어집니다. myString의 부분 문자열중 pat로 끝나는 가장 긴 부분 문자열을 찾아서 return 하는 solution 함수를 완성해 주세요.
  */
 
-function solution(my_string, pat) {
-    const s = my_string.lastIndexOf(pat)
-    return my_string.slice(0,s+pat.length)
+function solution(myString, pat) {
+  const index = myString.lastIndexOf(pat);
+  return myString.slice(0, index + pat.length);
 }
 
 console.log(solution("AbCdEFG", "dE"))
